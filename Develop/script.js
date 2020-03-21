d27552393d0e798f832c91369497505319157f95
Dazacher <dazacher2000@yahoo.com>
2020-03-21 05:37:01 -0700

    got random password generator to work.

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 var ansPasswordLength = prompt("How long do you want your password to be? You may chose a length of at Least 8 but no more than 128.");
 var passwordLength = parseInt(ansPasswordLength);
- // var passwordMinLimit = 8
-  // var passwordMaxLimit = 128
 var generatedPassword
 
  
@@ -11,66 +9,68 @@ var generatedPassword
 verifyPasswordLength(passwordLength);
 var password = generatePassword(password, passwordLength);
 
-  // var computerChoice = Math.floor(Math.random() * passwordLength) + 1;
   // Verify the length of the password requested is between 8 and 128 characters
 function verifyPasswordLength(length){
   for (var i = 0; i < 3; i++ ){
     if (length < 8){
       alert("Password length must be at least 8 charachters!");
-      var length = prompt("Please enter a password length between 8 and 128");
+      length = parseInt(length) = prompt("Please enter a password length between 8 and 128");
     }else if  (length > 128){
       alert("Password length can be no longer than 128 charachters!");
-      var newPasswordLength = prompt("Please enter a password length between 8 and 128");
+      length = parseInt(length) = prompt("Please enter a password length between 8 and 128");
+      // var newPasswordLength = prompt("Please enter a password length between 8 and 128");
     }else{
+      console.log(length)
       return length;
-    }
-    parseInt(newPasswordLength);
-    return newPasswordLength;
+    }  
   };
+  parseInt(length);
+  console.log(length)
+  return length;
 }
 
-function generatePassword(password, pwLength){
+function generatePassword(randomPassword, pwLength){
   
-  var ansSpecialChars = confirm("Do you want special characters in your password?");
-  var ansNumericChars = confirm("Do you want numbers in your password?"); 
+  var ansSpecialChars = confirm("Do you want Special Characters in your password?");
+  var ansNumericChars = confirm("Do you want Numbers in your password?"); 
   var ansUCLetters = confirm("Do you want Upper Case Letters in your password?");
-
-  var alphabetLCOptions = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  var alphabetUCOptions = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  var numericOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  var specialChars = ["!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|"]
-  var randomPassword
   
+  var ucLCNumericChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  
+  var lcSpecialNumericChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  
+  var numericLCUC = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+ 
+  var specialUCLCChars = ["!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", "A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+  
+  var allChars=["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ 
+  randomPassword = "";
+
     if (ansSpecialChars === true && ansUCLetters === false && ansNumericChars === true ){
-      // var passwordCharArray = [alphabetLCOptions, numericOptions, specialChars];
-      for ( i = 0; i <= pwLength; i++ ){
-      // var randomPassword = randomPassword.concat(passwordCharArray[Math.floor(Math.random() * pwLength) + 1]);
-      randomPassword = randomPassword.concat(ansSpecialChars[Math.floor(Math.random() * pwLength) + 1]);
       console.log(pwLength);
-      console.log(randomPassword);
-      randomPassword = randomPassword.concat(ansNumericChars[Math.floor(Math.random() * pwLength) + 1]);
-      console.log(pwLength);
-      console.log(randomPassword);
-      var randomPassword = randomPassword.concat(alphabetLCOptions[Math.floor(Math.random() * pwLength) + 1]);
-      console.log(pwLength);
-      conrandomPasswordsole.log(randomPassword);
-     } 
-    } else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === true ){
-      var passwordCharArray = [alphabetLCOptions, alphabetUCOptions, numericOptions];
-      randomPassword = password.concat(passwordCharArray[Math.floor(Math.random() * pwLength) + 1]);
-      console.log(pwLength);
-      console.log(randomPassword);
+      for ( i = 0; i < pwLength; i++ ){
+        randomPassword = randomPassword.concat(lcSpecialNumericChars[Math.floor(Math.random() * 49) + 1]);
+        console.log(randomPassword);
+      }
+    }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === true ){
+      for ( j = 0; j <= pwLength; j++ ){
+        randomPassword = randomPassword.concat(numericLCUC[Math.floor(Math.random() * 62) + 1]);
+        console.log(randomPassword);
+      }
     }else if(ansSpecialChars === true && ansUCLetters === true && ansNumericChars === false ){
-      var passwordCharArray = [alphabetLCOptions, alphabetUCOptions, specialChars];
-      // password = password.concat(passwordCharArray[Math.floor(Math.random() * pwLength) + 1]);
-      randomPassword = passwordCharArray[Math.floor(Math.random() * pwLength) + 1];
+      for ( k = 0; k <= pwLength; k++ ){
+        randomPassword = randomPassword.concat(specialUCLCChars[Math.floor(Math.random() * 66) + 1]);
+        console.log(pwLength);
+        console.log(randomPassword);
+      }
+    }else{
+      for ( l = 0; l <= pwLength; l++ ){
+        randomPassword = randomPassword.concat(allChars[Math.floor(Math.random() * 72) + 1]);
+        console.log(randomPassword);
+      }
       console.log(pwLength);
       console.log(randomPassword);
-    }else{
-    var passwordCharArray = [alphabetLCOptions, alphabetUCOptions, numericOptions, specialChars];
-    password = password.concat(passwordCharArray[Math.floor(Math.random() * pwLength) + 1]);
-    console.log(pwLength);
-    console.log(randomPassword);
     }
     console.log(pwLength);
     console.log(randomPassword);
