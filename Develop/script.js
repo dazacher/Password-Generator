227c3656489e722914e9cc95eea9c5251aa193e5
Dazacher <dazacher2000@yahoo.com>
2020-03-22 16:21:18 -0700

    Wrote Read Me, and added LC functionality.

--- Develop/script.js
+++ Develop/script.js
@@ -33,7 +33,8 @@ function writePassword(){
   function generatePassword(){
     // First ask how long they want their password to be
     var ansPasswordLength = prompt("How long do you want your password to be? You may chose a length of at Least 8 but no more than 128.");
-    var passwordLength = parseInt(ansPasswordLength);
+    var passwordLength = 0;
+    passwordLength = parseInt(ansPasswordLength);
   // Verify the length of the password is greather than or equal to 8 and less than or equal to 128
     passwordLength = verifyPasswordLength(passwordLength);
     console.log(passwordLength);
@@ -41,17 +42,32 @@ function writePassword(){
     var passwordMin = 8;
     var passwordMax = 128;
     // If the password has passed the lenght test ask what kind of characters they want in it
-    if ( passwordLength >= 8 && passwordLength <= 128){
+    if ( passwordLength >= passwordMin && passwordLength <= passwordMax){
       console.log("Inside min max if statement");
       console.log(passwordLength)
       // Ask user some questions about what kind of chars they want in their password
       var ansSpecialChars = confirm("Do you want Special Characters in your password?");
       var ansNumericChars = confirm("Do you want Numbers in your password?"); 
       var ansUCLetters = confirm("Do you want Upper Case Letters in your password?");
+      var ansLCLetters = confirm("Do you want Lower Case letters in your password?")
     
     var lcSpecialNumericChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+    var specialNumericChars =[ "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    var special = ["!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|"]
+
+    var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    var lc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    var uc = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
     var lcNumericChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    var ucNumericChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    var ucSpecialChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|"]
 
     var specialLCChars = ["!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     
@@ -59,42 +75,72 @@ function writePassword(){
    
     var specialUCLCChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-    ucLCChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var ucLCChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     
-    var allChars=["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var allChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
    
     randomPassword = "";
    
-      if (ansSpecialChars === true && ansUCLetters === false && ansNumericChars === true ){
+      if (ansSpecialChars === true && ansUCLetters === false && ansNumericChars === true && ansLCLetters === true ){
         console.log(passwordLength);
         for ( m = 0; m < passwordLength; m++ ){
-          randomPassword = randomPassword.concat(lcSpecialNumericChars[Math.floor(Math.random() * 49) + 1]);
+          randomPassword = randomPassword.concat(lcSpecialNumericChars[Math.floor(Math.random() * 49)]);
           console.log(randomPassword);
         }
-      } else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === true ){
+      } else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === true && ansLCLetters === true  ){
         for ( j = 0; j < passwordLength; j++ ){
-          randomPassword = randomPassword.concat(numericLCUC[Math.floor(Math.random() * 62) + 1]);
+          randomPassword = randomPassword.concat(numericLCUC[Math.floor(Math.random() * 62)]);
           console.log(randomPassword);
         }
-      }else if(ansSpecialChars === true && ansUCLetters === true && ansNumericChars === false ){
+      }else if(ansSpecialChars === true && ansUCLetters === true && ansNumericChars === false && ansLCLetters === true ){
         for ( k = 0; k < passwordLength; k++ ){
-          randomPassword = randomPassword.concat(specialUCLCChars[Math.floor(Math.random() * 66) + 1]);
+          randomPassword = randomPassword.concat(specialUCLCChars[Math.floor(Math.random() * 66)]);
           console.log(passwordLength);
           console.log(randomPassword);
         }
-      }else if(ansSpecialChars === false && ansUCLetters === false && ansNumericChars === true ){
-        for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(lcNumericChars[Math.floor(Math.random() * 36) + 1]);
+      }else if(ansSpecialChars === false && ansUCLetters === false && ansNumericChars === true && ansLCLetters === true ){
+        for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(lcNumericChars[Math.floor(Math.random() * 36)]);
           console.log(randomPassword);
         }
-      }else if(ansSpecialChars === true && ansUCLetters === false && ansNumericChars === false ){
-        for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(specialLCChars[Math.floor(Math.random() * 40) + 1]);
+      }else if(ansSpecialChars === true && ansUCLetters === false && ansNumericChars === false && ansLCLetters === true ){
+        for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(specialLCChars[Math.floor(Math.random() * 40)]);
         console.log(randomPassword);
         }
-      }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === false ){
-          for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(ucLCChars[Math.floor(Math.random() * 40) + 1]);
+      }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === false && ansLCLetters === true ){
+          for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(ucLCChars[Math.floor(Math.random() * 52)]);
           console.log(randomPassword);
         }
-      }else{
+      }else if(ansSpecialChars === true && ansUCLetters === false && ansNumericChars === true && ansLCLetters === false ){
+        for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(specialNumericChars[Math.floor(Math.random() * 24)]);
+        console.log(randomPassword);
+      }
+    }else if(ansSpecialChars === true && ansUCLetters === false && ansNumericChars === false && ansLCLetters === false ){
+      for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(special[Math.floor(Math.random() * 14)]);
+      console.log(randomPassword);
+      }
+    }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === false && ansLCLetters === true ){
+      for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(lc[Math.floor(Math.random() * 26)]);
+      console.log(randomPassword);
+      }
+    }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === false && ansLCLetters === false ){
+      for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(uc[Math.floor(Math.random() * 26)]);
+      console.log(randomPassword);
+      }
+    }else if(ansSpecialChars === false && ansUCLetters === false && ansNumericChars === true && ansLCLetters === false ){
+      for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(numeric[Math.floor(Math.random() * 10)]);
+      console.log(randomPassword);
+      }
+    }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === true && ansLCLetters === false ){
+      for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(ucNumericChars[Math.floor(Math.random() * 36)]);
+      console.log(randomPassword);
+      }
+    }else if(ansSpecialChars === true && ansUCLetters === true && ansNumericChars === false && ansLCLetters === false ){
+      for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(ucSpecialChars[Math.floor(Math.random() * 40)]);
+      console.log(randomPassword);
+      }
+    }else if(ansSpecialChars === false && ansUCLetters === false && ansNumericChars === false && ansLCLetters === false ){
+      alert("You must chose one of the options for Special Characters, Numbers, Lower Case, or Upper Case letters.")
+    }else{
         for ( l = 0; l < passwordLength; l++ ){
           console.log(passwordLength);
           randomPassword = randomPassword.concat(allChars[Math.floor(Math.random() * 72) + 1]);
@@ -102,16 +148,25 @@ function writePassword(){
         }
         console.log(randomPassword);
       }
+    
       console.log(randomPassword);
       return randomPassword;
   }
 };
   // Generate the password
   var password = generatePassword();
-  // Write password to the #password input
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if(password === undefined){
+    console.log(password);
+    // Print out error message due to incorrect length being entered.
+    var passwordText = document.querySelector("#password");
+    passwordText.value = "Incorrect Password length.";    
+  }else{
+    var passwordText = document.querySelector("#password");
+    // Write password to the #password input
+    passwordText.value = password;
+    console.log(password);
+  } 
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Password-Generator
+This program is a password generator.
+
+The user presses a 'Generate Password' button and is first asked how long they wish thier password to be.
+The password must be greater than or equal to 8 and less than or equal to 128 charachters in length.  The length of 
+the password if verified. If an incorrect length is not entered after 4 tries a message will be displayed telling
+the user that the incorrect length has been entered and they can not get a password unless they press the 
+Generate Password button again.
+
+After a correct lenght is entered, the user is asked to confirm whether they want Speical Characters, Numbers,
+Lower Case, and Upper Case letters. Once all questions are answered, the password is generated. If no option is
+entered for Special Characters, Numbers, Lower Case, or Upper Case letters, an alert will pop-up notifying the
+user and they will have to start all over again.
+
+https://dazacher.github.io/Password-Generator/
