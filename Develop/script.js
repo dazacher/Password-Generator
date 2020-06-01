9b1c6ce5af24fcf8d62d82cf9b6105da077e757b
Dazacher <dazacher2000@yahoo.com>
2020-06-01 10:29:05 -0700

    Removed old code.

--- Develop/script.js
+++ Develop/script.js
@@ -51,34 +51,6 @@ function writePassword() {
       var ansUCLetters = confirm("Do you want Upper Case Letters in your password?");
       var ansLCLetters = confirm("Do you want Lower Case letters in your password?")
 
-      // var lcSpecialNumericChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-      // var specialNumericChars =[ "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-      // var special = ["!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|"]
-
-      // var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-      // var lc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-      // var uc = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-      // var lcNumericChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-      // var ucNumericChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-      // var ucSpecialChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|"]
-
-      // var specialLCChars = ["!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-      // var numericLCUC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-      // var specialUCLCChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-      // var ucLCChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-      // var allChars = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "#", "$", "%", "&", "*", "+" , "-", "/", "<", "?", "@", "[", "|", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
       var specialChars = "/<?[|~!@#$%^&*()-_+=";
       var ucChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       var lcChars = "abcdefghijklmnopqrstuvwxyz";
@@ -119,75 +91,6 @@ function writePassword() {
           console.log("randomPassword " + randomPassword)
         };
       }
-
-
-      //   if (ansSpecialChars === true && ansUCLetters === false && ansNumericChars === true && ansLCLetters === true ){
-      //     console.log(passwordLength);
-      //     for ( m = 0; m < passwordLength; m++ ){
-      //       randomPassword = randomPassword.concat(lcSpecialNumericChars[Math.floor(Math.random() * 49)]);
-      //       console.log(randomPassword);
-      //     }
-      //   } else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === true && ansLCLetters === true  ){
-      //     for ( j = 0; j < passwordLength; j++ ){
-      //       randomPassword = randomPassword.concat(numericLCUC[Math.floor(Math.random() * 62)]);
-      //       console.log(randomPassword);
-      //     }
-      //   }else if(ansSpecialChars === true && ansUCLetters === true && ansNumericChars === false && ansLCLetters === true ){
-      //     for ( k = 0; k < passwordLength; k++ ){
-      //       randomPassword = randomPassword.concat(specialUCLCChars[Math.floor(Math.random() * 66)]);
-      //       console.log(passwordLength);
-      //       console.log(randomPassword);
-      //     }
-      //   }else if(ansSpecialChars === false && ansUCLetters === false && ansNumericChars === true && ansLCLetters === true ){
-      //     for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(lcNumericChars[Math.floor(Math.random() * 36)]);
-      //       console.log(randomPassword);
-      //     }
-      //   }else if(ansSpecialChars === true && ansUCLetters === false && ansNumericChars === false && ansLCLetters === true ){
-      //     for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(specialLCChars[Math.floor(Math.random() * 40)]);
-      //     console.log(randomPassword);
-      //     }
-      //   }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === false && ansLCLetters === true ){
-      //       for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(ucLCChars[Math.floor(Math.random() * 52)]);
-      //       console.log(randomPassword);
-      //     }
-      //   }else if(ansSpecialChars === true && ansUCLetters === false && ansNumericChars === true && ansLCLetters === false ){
-      //     for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(specialNumericChars[Math.floor(Math.random() * 24)]);
-      //     console.log(randomPassword);
-      //   }
-      // }else if(ansSpecialChars === true && ansUCLetters === false && ansNumericChars === false && ansLCLetters === false ){
-      //   for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(special[Math.floor(Math.random() * 14)]);
-      //   console.log(randomPassword);
-      //   }
-      // }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === false && ansLCLetters === true ){
-      //   for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(lc[Math.floor(Math.random() * 26)]);
-      //   console.log(randomPassword);
-      //   }
-      // }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === false && ansLCLetters === false ){
-      //   for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(uc[Math.floor(Math.random() * 26)]);
-      //   console.log(randomPassword);
-      //   }
-      // }else if(ansSpecialChars === false && ansUCLetters === false && ansNumericChars === true && ansLCLetters === false ){
-      //   for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(numeric[Math.floor(Math.random() * 10)]);
-      //   console.log(randomPassword);
-      //   }
-      // }else if(ansSpecialChars === false && ansUCLetters === true && ansNumericChars === true && ansLCLetters === false ){
-      //   for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(ucNumericChars[Math.floor(Math.random() * 36)]);
-      //   console.log(randomPassword);
-      //   }
-      // }else if(ansSpecialChars === true && ansUCLetters === true && ansNumericChars === false && ansLCLetters === false ){
-      //   for ( n = 0; n < passwordLength; n++ ){randomPassword = randomPassword.concat(ucSpecialChars[Math.floor(Math.random() * 40)]);
-      //   console.log(randomPassword);
-      //   }
-      // }else if(ansSpecialChars === false && ansUCLetters === false && ansNumericChars === false && ansLCLetters === false ){
-      //   alert("You must chose one of the options for Special Characters, Numbers, Lower Case, or Upper Case letters.")
-      // }else{
-      //     for ( l = 0; l < passwordLength; l++ ){
-      //       console.log(passwordLength);
-      //       randomPassword = randomPassword.concat(allChars[Math.floor(Math.random() * 72) + 1]);
-      //       console.log(randomPassword);
-      //     }
-      //     console.log(randomPassword);
-      //   }
 
       console.log(randomPassword);
       return randomPassword;
